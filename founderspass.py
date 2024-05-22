23c252c49faffa45029069649e0dff9e004a34a5
CHRISTOPHER GOLD <cgoldaz@gmail.com>
2024-05-22 21:35:11 +0800

    finally working!

--- founderspass.py
+++ founderspass.py
@@ -27,20 +27,31 @@ def m():
             main.ChangeMetadata.__init__(self)
             main.Nft.__init__(self, metadata, ledger, token_metadata)
             main.Admin.__init__(self, administrator)
-        
-        @sp.entry_point
-        def mint(self, params):
+
+        @sp.entrypoint
+        def mint(self, batch):
+            """Admin can mint new or existing tokens."""
             # Define the NFT metadata
             nft_metadata = {
-                 "" : sp.scenario_utils.bytes_of_string("ipfs://bafkreierffftgdmhrwqca6hkjxm7m5kmtbaoglkz32tt2tlepu7zxic72q")
+                "" : sp.bytes("0x697066733a2f2f6261666b726569726666667464676d68727771636136686b6a786d376d356b6d7462616f676c6b7a3332747432746c657075377a786963373271")
             }
             
-            # Call the original mint function with nft_metadata
-            main.MintNft.mint(sp.record(
-                token_id=params.token_id,
-                owner=params.owner,
-                token_info=nft_metadata
-            ))
+            sp.cast(
+                batch,
+                sp.list[
+                    sp.record(
+                        to_=sp.address,
+                    ).layout(("to_"))
+                ],
+            )
+            assert self.is_administrator_(), "FA2_NOT_ADMIN"
+            for action in batch:
+                token_id = self.data.next_token_id
+                self.data.token_metadata[token_id] = sp.record(
+                    token_id=token_id, token_info=nft_metadata
+                )
+                self.data.ledger[token_id] = action.to_
+                self.data.next_token_id += 1
 
 @sp.add_test()
 def test():
@@ -62,10 +73,29 @@ def test():
         administrator=owner,
         metadata=contract_metadata,
         ledger=ledger,
-        token_metadata=[sp.list([
-            sp.map({})
-        ])]
+        token_metadata=[]
     )
 
     scenario.h2("Contract")
     scenario += c1
+
+    # Create a single minting action for one NFT
+    batch = sp.list([sp.record(to_=owner)])
+
+    scenario.h2("Mint 1 NFT to the owner")
+
+    # Call the mint entry point with the single action
+    c1.mint(batch, _sender=owner)
+
+    # Log the value of the ledger to verify it is correct
+    scenario.h3("Ledger after minting")
+    scenario.show(c1.data.ledger)
+
+    # Create a batch of minting actions for 300 NFTs
+    batch = sp.list([sp.record(to_=owner) for _ in range(300)])
+
+    scenario.h4("Mint 300 NFTs to the owner")
+
+    # Call the mint entry point with the batch
+    c1.mint(batch, _sender=owner)
+    scenario.show(c1.data.ledger)
